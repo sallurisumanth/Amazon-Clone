ddb8500dad7433b85c1826d785b8d8061ee902e1
SalluriSumanth <sallurisumanth05@gmail.com>
2025-02-12 19:07:28 -0600

    Create Order using Backend

--- scripts/checkout/paymentSummary.js
+++ scripts/checkout/paymentSummary.js
@@ -1,7 +1,8 @@
 import { cart } from "../../data/cart.js";
 import { getProduct } from "../../data/products.js";
 import { getDeliveryOption } from "../../data/delivertyOptiopns.js";
-import {formatCurrency} from "../utils/money.js"
+import {formatCurrency} from "../utils/money.js";
+import { addOrder } from "../../data/orders.js";
 
 export function renderPaymentSummary(){
 
@@ -25,49 +26,71 @@ export function renderPaymentSummary(){
 
  
   const paynmentSummaryHTML = `
-            <div class="payment-summary-title">
-                Order Summary
-              </div>
+<div class="payment-summary-title">
+    Order Summary
+  </div>
 
-              <div class="payment-summary-row">
-                <div>Items (3):</div>
-                <div class="payment-summary-money">$${formatCurrency(productPriceCents)}
-                </div>
-              </div>
+  <div class="payment-summary-row">
+    <div>Items (3):</div>
+    <div class="payment-summary-money">$${formatCurrency(productPriceCents)}
+    </div>
+    </div>
 
-              <div class="payment-summary-row">
-                <div>Shipping &amp; handling:</div>
-                <div class="payment-summary-money">
-                $${formatCurrency(shippingPriceCents)
-                }
-                
-                </div>
-              </div>
+    <div class="payment-summary-row">
+      <div>Shipping &amp; handling:</div>
+      <div class="payment-summary-money">
+      $${formatCurrency(shippingPriceCents)
+      }
+      
+      </div>
+    </div>
 
-              <div class="payment-summary-row subtotal-row">
-                <div>Total before tax:</div>
-                <div class="payment-summary-money">
-                $${formatCurrency(totalBeforeTaxCents)}
-                </div>
-              </div>
+    <div class="payment-summary-row subtotal-row">
+      <div>Total before tax:</div>
+      <div class="payment-summary-money">
+      $${formatCurrency(totalBeforeTaxCents)}
+      </div>
+    </div>
 
-              <div class="payment-summary-row">
-                <div>Estimated tax (10%):</div>
-                <div class="payment-summary-money">$${(formatCurrency(estimatedTaxCents))}</div>
-              </div>
+    <div class="payment-summary-row">
+      <div>Estimated tax (10%):</div>
+      <div class="payment-summary-money">$${(formatCurrency(estimatedTaxCents))}</div>
+    </div>
 
-              <div class="payment-summary-row total-row">
-                <div>Order total:</div>
-                <div class="payment-summary-money">$${(formatCurrency(totalCents))}</div>
-              </div>
+    <div class="payment-summary-row total-row">
+      <div>Order total:</div>
+      <div class="payment-summary-money">$${(formatCurrency(totalCents))}</div>
+    </div>
 
-              <button class="place-order-button button-primary">
-                Place your order
-              </button>`;
+    <button class="place-order-button button-primary js-place-order">
+      Place your order
+    </button>`;
     
     document.querySelector('.js-payment-summary')
     .innerHTML = paynmentSummaryHTML;
 
+    document.querySelector('.js-place-order')
+    .addEventListener('click', async() => {
+
+      try {
+        const response =   await fetch('https://supersimplebackend.dev/orders', {
+          method: 'POST',
+          headers: {
+            'Content-Type' : 'application/json' 
+          },
+          body: JSON.stringify({
+            cart: cart
+          })
+        });
+  
+        const order = await response.json();
+        addOrder(order);
+      } catch (error){
+        console.log('Unexpected Error. Try Again Later.');
+      }
+    
+      window.location.href = 'orders.html';
+    });
 
 
 }
